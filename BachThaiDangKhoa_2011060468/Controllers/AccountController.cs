2b4688589cdbeeffebfcc9a969e55bd48fc214cc
dangkhoabach <dangkhoabach2002@gmail.com>
2023-03-13 17:10:11 +0700

    Update code 13/03/2023

--- BachThaiDangKhoa_2011060468/Controllers/AccountController.cs
+++ BachThaiDangKhoa_2011060468/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace BachThaiDangKhoa_2011060468.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Name = model.Name };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- BachThaiDangKhoa_2011060468/Models/AccountViewModels.cs
+++ BachThaiDangKhoa_2011060468/Models/AccountViewModels.cs
@@ -79,6 +79,10 @@ namespace BachThaiDangKhoa_2011060468.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
     }
 
     public class ResetPasswordViewModel
